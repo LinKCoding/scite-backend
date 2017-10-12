6374aa847341b31d9e641e3f79e6fdea36feff5b
linkcoding <kenny.lin.91@gmail.com>
2017-10-12 17:41:41 -0400

    removed name from json response

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::AuthController < ApplicationController
     if @user && @user.authenticate(params[:password])
       #YOU ARE WHO YOU SAY YOU ARE
       token = encode_token({ user_id: @user.id})
-      render json: {name: @user.first_name, jwt: token}
+      render json: { jwt: token}
     else
       render json: { error: "could not log in"}
     end
