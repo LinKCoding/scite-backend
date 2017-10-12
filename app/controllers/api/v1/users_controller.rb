b8b933f1a598c9ee18646814207501a483dd9531
linkcoding <kenny.lin.91@gmail.com>
2017-10-12 09:35:03 -0400

    set up back end for sign ups

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::UsersController < ApplicationController
+
+  def create
+    @user = User.new(first_name: params[:first_name], last_name: params[:last_name],  email: params[:email], password: params[:password])
+    if @user.save
+      render json: @user
+    end
+  end
+
+  def index
+  end
+
+
+  def login
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      render json: @user
+    else
+      render json: {status: "error", code: 401}
+    end
+  end
+
+end
