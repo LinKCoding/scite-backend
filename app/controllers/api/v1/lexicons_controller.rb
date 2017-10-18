551e904d2b63ab3d14ead04f2d2922ca2422650a
linkcoding <kenny.lin.91@gmail.com>
2017-10-18 14:48:13 -0400

    fixed lexicon controller

--- app/controllers/api/v1/lexicons_controller.rb
+++ app/controllers/api/v1/lexicons_controller.rb
@@ -2,13 +2,13 @@ class Api::V1::LexiconsController < ApplicationController
   before_action :authorized
 
   def create
+
     if @user
-      @note.find_by(id: params[:note_id])
-      @lexicon = @note.lexicons
-      newWord = Lexicon.create(word: params[:word, definition: params[:definition]], note_id: @note.id)
-      @lexicon << newWord
-      byebug
-      render json: @lexicon
+
+      @note = Note.find_by(id: params[:note_id])
+      # @lexicon = @note.lexicons
+      @newWord = Lexicon.create(word: params[:word], definition: params[:definition], note_id: @note.id)
+      render json: @newWord
     end
   end
 
