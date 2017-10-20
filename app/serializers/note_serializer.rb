de6c2684d88fc78acb7c34539ec9b8e3172f9a64
linkcoding <kenny.lin.91@gmail.com>
2017-10-20 08:59:10 -0400

    added to note serializer

---
+++ app/serializers/note_serializer.rb
@@ -0,0 +1,12 @@
+class NoteSerializer < ActiveModel::Serializer
+  attributes :id, :article_id, :content, :user_id, :article_name, :date_created
+
+  def article_name
+    article = Article.find_by(id: object.article_id)
+    article.name
+  end
+
+  def date_created
+    object.created_at.to_date
+  end
+end
