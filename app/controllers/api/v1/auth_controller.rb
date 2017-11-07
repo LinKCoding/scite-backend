4f2c92505d6629fc1b38a8958630df7d3325fee2
linkcoding <kenny.lin.91@gmail.com>
2017-11-07 11:49:03 -0500

    resetting

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -3,9 +3,7 @@ class Api::V1::AuthController < ApplicationController
   skip_before_action :authorized, only: [:create]
   def create
     @user = User.find_by(email: params[:email])
-
     if @user && @user.authenticate(params[:password])
-      #YOU ARE WHO YOU SAY YOU ARE
       token = encode_token({ user_id: @user.id})
       render json: { jwt: token, user_first_name: @user.first_name}
     else
